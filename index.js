430e8c9c93639efa173d3f59120dac138c627d7f
valminjr <junior.valmir6@gmail.com>
2019-07-11 00:21:36 -0300

    chore: change in the initial route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const server = express();
 
 server.get("/teste", (req, res) => {
-  return res.json({ message: "Oi" });
+  return res.json({ message: "Oi oi oi" });
 });
 
 server.listen(3000);
